2a376e1129e88c81d6d82073fb6cd3ef16ab8fc4
fazal199 <fazalhussain1441@gmail.com>
2023-10-13 20:02:41 +0530

    isha commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -46,19 +46,12 @@ function Navbar({ titlename, flink, slink}) {
         <div className={`${menu} w-full md:block md:w-auto`} id="navbar-default">
               <ul className="flex flex-col p-4 mt-4 text-lg font-semibold text-center border border-gray-100 rounded-lg md:p-0 md:flex-row md:space-x-8 md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 gap-7">
                 <li>
-                  <a
-                    className="block py-2 pl-3 pr-4 text-white rounded md:bg-transparent md:p-0 dark:text-white"
-                    aria-current="page"
-                  >
-                  <Link to="/"> {flink}</Link>
-                  </a>
+                  <Link  className="block py-2 pl-3 pr-4 text-white rounded md:bg-transparent md:p-0 dark:text-white"
+                    aria-current="page" to="/"> {flink}</Link>
                 </li>
                 <li>
-                  <a
-                    className="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                  >
-                  <Link to="/about">{slink}</Link>
-                  </a>
+                 
+                  <Link className="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" to="/about">{slink}</Link>
                 </li>
     
               </ul>
